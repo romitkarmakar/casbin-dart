5561756f86c98b79e0f1d6b3da0131d6fe0f44c5
Romit Karmakar <romitkarmakar@gmail.com>
2020-04-13 11:21:46 +0530

    Added tests

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:convert';
 
+/// This class reads the .conf file to generate model.
 class Config {
   final String DEFAULT_SECTION = "default";
   final String DEFAULT_COMMENT = "#";
@@ -9,16 +10,24 @@ class Config {
 
   Map<String, Map<String, String>> data;
 
-  Config(String confName) {
+  /// Initialises a empty config
+  Config() {
+    data = new Map();
+  }
+
+  /// Intialises Config object from file
+  Config.fromFile(String confName) {
     data = new Map();
     parse(confName);
   }
 
+  /// Initialises Config object from String object.
   Config.fromText(String text) {
     data = new Map();
     parseBuffer(text);
   }
 
+  /// Adds value to a section.
   addConfig(String section, String option, String value) {
     if (section == "") section = DEFAULT_SECTION;
     if (data[section] == null) data[section] = new Map();
@@ -33,47 +42,68 @@ class Config {
   }
 
   parseBuffer(String buf) {
-    String section;
-    int lineNum = 0;
-    bool canWrite = false;
+    String section = "";
     String temp = "";
     LineSplitter ls = new LineSplitter();
+    bool canWrite = false;
 
     List<String> buffer = ls.convert(buf);
 
     buffer.forEach((String line) {
-      line = line.trim();
-      if (canWrite) write(section, lineNum, line);
-      lineNum++;
+      if (canWrite) {
+        write(section, temp);
+        temp = "";
+        canWrite = false;
+      }
 
-      if (line.startsWith(DEFAULT_COMMENT_SEM) || line.startsWith(DEFAULT_COMMENT)) return;
-      else if(line.startsWith("[") && line.endsWith("]")) {
-        write(section, lineNum, temp);
+      line = line.trim();
+
+      if (line.startsWith(DEFAULT_COMMENT_SEM) ||
+          line.startsWith(DEFAULT_COMMENT))
+        return;
+      else if (line.startsWith("[") && line.endsWith("]")) {
         section = line.substring(1, line.length - 1);
-      } else if(line.endsWith(DEFAULT_MULTILINE_SEPARATOR)) temp += line.substring(0, line.length - 1);
-      else temp += line;
+      } else if (line.endsWith(DEFAULT_MULTILINE_SEPARATOR))
+        temp += line.substring(0, line.length - 2);
+      else {
+        temp += line;
+        canWrite = true;
+      }
     });
 
-    write(section, lineNum, temp);
+    write(section, temp);
   }
 
-  write(String section, int linenum, String b) {
-    List<String> optionVal = b.split("=");
+  write(String section, String b) {
+    if (b == "") return;
+    List<String> optionVal = new List<String>();
+    int index = 0;
+
+    while(index < b.length) {
+      if(b[index] == "=") break;
+      index++;
+    }   
+
+    optionVal.add(b.substring(0, index)); 
+    optionVal.add(b.substring(index+1, b.length));
+
     String option = optionVal[0].trim();
     String value = optionVal[1].trim();
 
     addConfig(section, option, value);
   }
 
+  /// Sets the config data specified by the key value.
   set(String key, String value) {
     String section;
     String option;
 
     List<String> keys = key.split("::");
-    if(keys.length >= 2) {
+    if (keys.length >= 2) {
       section = keys[0];
       option = keys[1];
-    } else option = keys[0];
+    } else
+      option = keys[0];
 
     addConfig(section, option, value);
   }
@@ -83,10 +113,11 @@ class Config {
     String option;
 
     List<String> keys = key.split("::");
-    if(keys.length >= 2) {
+    if (keys.length >= 2) {
       section = keys[0];
       option = keys[1];
-    } else option = keys[0];
+    } else
+      option = keys[0];
 
     String value = data[section][option];
 
