56422123f3ddfe17b7bff9407d7a8125b54aa55f
Romit Karmakar <romitkarmakar@gmail.com>
2020-04-13 07:13:47 +0530

    Added basic structure

---
+++ src/effect/effector.dart
@@ -0,0 +1,50 @@
+import 'dart:typed_data';
+
+enum Effect { Allow, Indeterminate, Deny }
+
+class Effector {
+  static bool mergeEffects(
+      String expr, List<Effect> effects, Float64List results) {
+    bool result = false;
+    if (expr == "some(where (p_eft == allow))") {
+      result = false;
+      effects.forEach((Effect eft) {
+        if (eft == Effect.Allow) {
+          result = true;
+        }
+      });
+    } else if (expr == "!some(where (p_eft == deny))") {
+      result = true;
+      effects.forEach((Effect eft) {
+        if (eft == Effect.Deny) {
+          result = false;
+        }
+      });
+    } else if (expr ==
+        "some(where (p_eft == allow)) && !some(where (p_eft == deny))") {
+      result = false;
+      effects.forEach((Effect eft) {
+        if (eft == Effect.Allow) {
+          result = true;
+        } else if (eft == Effect.Deny) {
+          result = false;
+        }
+      });
+    } else if (expr == "priority(p_eft) || deny") {
+      result = false;
+      effects.forEach((Effect eft) {
+        if (eft != Effect.Indeterminate) {
+          if (eft == Effect.Allow) {
+            result = true;
+          } else {
+            result = false;
+          }
+        }
+      });
+    } else {
+      return false;
+    }
+
+    return result;
+  }
+}
